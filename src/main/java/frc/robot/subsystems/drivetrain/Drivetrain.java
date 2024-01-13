c4b90cc81985d9889409ac52d5c64ac063bd3292
Daniel Quednow <dqued56@tscstudents.net>
2024-01-13 09:06:37 -0500

    Add drive train from last year
    Added logging

---
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems.drivetrain;
+
+import java.util.List;
+
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
+
+import frc.robot.subsystems.DrivetrainInputsAutoLogged;
+import frc.robot.utils.Loggable;
+import frc.robot.utils.Pigeon;
+import frc.robot.utils.SwerveModule;
+import frc.team4272.swerve.utils.SwerveDriveBase;
+import frc.team4272.swerve.utils.SwerveModuleBase.PositionedSwerveModule;
+
+import static frc.robot.constants.HardwareMap.*;
+import static frc.robot.constants.RobotConstants.DrivetrainConstants.*;
+
+public class Drivetrain extends SwerveDriveBase<Pigeon, SwerveModule> implements Loggable {
+    @AutoLog
+    public static class DrivetrainInputs {
+        public double xPosition;
+        public double yPosition;
+        public double thetaRadians;
+    }
+
+    private DrivetrainInputsAutoLogged drivetrainInputs;
+
+    public Drivetrain() {
+        super(
+            new Pigeon(PIGEON_ID),
+            SwerveModule.class,
+            List.of(
+                new PositionedSwerveModule<SwerveModule>(new SwerveModule(FRONT_LEFT_MODULE_ID,  FRONT_LEFT_OFFSET),  FRONT_LEFT_POSITION),
+                new PositionedSwerveModule<SwerveModule>(new SwerveModule(FRONT_RIGHT_MODULE_ID, FRONT_RIGHT_OFFSET), FRONT_RIGHT_POSITION),
+                new PositionedSwerveModule<SwerveModule>(new SwerveModule(BACK_LEFT_MODULE_ID,   BACK_LEFT_OFFSET),   BACK_LEFT_POSITION),
+                new PositionedSwerveModule<SwerveModule>(new SwerveModule(BACK_RIGHT_MODULE_ID,  BACK_RIGHT_OFFSET),  BACK_RIGHT_POSITION)
+            )
+        );
+
+        drivetrainInputs = new DrivetrainInputsAutoLogged();
+
+        setMaxSpeeds(14.5);
+    }
+
+    @Override
+    public void log(String subdirectory, String humanReadableName) {
+        for(int i = 0; i < modules.length; i++) {
+            modules[i].log(subdirectory + "/" + humanReadableName, "Module" + i);
+        }
+
+        gyroscope.log(subdirectory + "/" + humanReadableName, "Pigeon");
+
+        drivetrainInputs.xPosition = 0;
+        drivetrainInputs.yPosition = 0;
+        drivetrainInputs.thetaRadians = 0;
+
+        Logger.processInputs(subdirectory + "/" + humanReadableName, drivetrainInputs);
+    }
+
+    @Override
+    public void periodic() {
+        log("Subsystems", "Drivetrain");
+    }
+}
