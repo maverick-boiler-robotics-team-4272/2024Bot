80aa13296c835fda2c658daf56b5706721617e37
Daniel Quednow <dqued56@tscstudents.net>
2024-01-13 13:25:50 -0500

    Finish implementation of Pigeon
    Added looping to make sure that the yaw begins between 0 and 360
    Added an offset so that way the rotation of the robot can be set
    Changed outputs to include both the IMU reading, and the
    getRotation method output
    (In lined to not have to wrap and unwrap in a Rotation2d object)

--- src/main/java/frc/robot/utils/Pigeon.java
+++ src/main/java/frc/robot/utils/Pigeon.java
@@ -6,35 +6,41 @@ import org.littletonrobotics.junction.Logger;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.team4272.globals.Gyroscope;
+import frc.team4272.globals.MathUtils;
 
 public class Pigeon extends Pigeon2 implements Gyroscope, Loggable {
 
     @AutoLog
     public static class PigeonInputs {
-        public double gyroscopeAngleRadians;
+        public double gyroscopeAngleDegrees;
+        public double outputAngleDegrees;
     }
 
     private PigeonInputsAutoLogged pigeonInputs;
+    private double offset;
 
     public Pigeon(int id) {
         super(id);
 
         pigeonInputs = new PigeonInputsAutoLogged();
+
+        setYaw(MathUtils.euclideanModulo(getYaw().getValueAsDouble(), 360.0));
     }
 
     @Override
     public Rotation2d getRotation() {
-        return Rotation2d.fromDegrees(-getYaw().getValueAsDouble());
+        return Rotation2d.fromDegrees(-getYaw().getValueAsDouble() + offset);
     }
 
     @Override
     public void setRotation(Rotation2d rotation) {
-        // TODO Auto-generated method stub
+        offset = getYaw().getValueAsDouble() - rotation.getDegrees();
     }
 
     @Override
     public void log(String subdirectory, String humanReadableName) {
-        pigeonInputs.gyroscopeAngleRadians = getRotation().getRadians();
+        pigeonInputs.gyroscopeAngleDegrees = getYaw().getValueAsDouble();
+        pigeonInputs.outputAngleDegrees = -getYaw().getValueAsDouble() + offset;
 
         Logger.processInputs(subdirectory + "/" + humanReadableName, pigeonInputs);
     }
