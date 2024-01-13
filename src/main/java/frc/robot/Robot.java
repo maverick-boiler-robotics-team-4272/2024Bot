278d4de4c7e97459c242e767229ed0a4384bc27a
Daniel Quednow <dqued56@tscstudents.net>
2024-01-13 08:59:27 -0500

    Change logging location

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -40,7 +40,7 @@ public class Robot extends LoggedRobot {
         Logger.recordMetadata("ProjectName", "MyProject"); // Set a metadata value
 
         if (isReal()) {
-            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+            Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/Logs")); // Log to a USB stick ("/U/logs")
             Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
             new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
         } else {
