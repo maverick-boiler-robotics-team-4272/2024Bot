45fb3803e0e3f2082920a6ae12a12f52a5a53948
Daniel Quednow <dqued56@tscstudents.net>
2024-01-18 19:28:04 -0500

    Add trigger to reset odometry
    Added a button to allow us to reset the odometry to a
    known point, i.e. the center of the field.
    Drive to the spike, hit y, odometry reset.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -63,6 +64,10 @@ public class RobotContainer {
 
         new Trigger(driveController.getButton("b")::get).onTrue(
             new ResetHeadingState(drivetrain)
+        );
+
+        new Trigger(driveController.getButton("y")::get).onTrue(
+            new InstantCommand(drivetrain::setToZero, drivetrain)
         );
     }
 
