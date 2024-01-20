6c83c4229b991e25c8e036e433513cd9556905c4
Daniel Quednow <dqued56@tscstudents.net>
2024-01-20 09:59:56 -0500

    Add auto table to telemetry constants
    Added an auto selection table to the telemetry constants
    Added the private constructors and errors to keep
    with the previously established conventions of this codebase

--- src/main/java/frc/robot/constants/TelemetryConstants.java
+++ src/main/java/frc/robot/constants/TelemetryConstants.java
@@ -1,9 +1,25 @@
 package frc.robot.constants;
 
 import frc.robot.limelight.Limelight;
+import frc.robot.utils.ShuffleboardTable;
 
 public class TelemetryConstants {
+    private TelemetryConstants() {
+        throw new UnsupportedOperationException("Cannot construct a constants class");
+    }
+
     public static class Limelights {
+        private Limelights() {
+            throw new UnsupportedOperationException("Cannot construct a constants class");
+        }
         public static final Limelight CENTER_LIMELIGHT = Limelight.getLimelight("center");
+    }
+
+    public static class ShuffleboardTables {
+        private ShuffleboardTables() {
+            throw new UnsupportedOperationException("Cannot construct a constants class");
+        }
+
+        public static final ShuffleboardTable AUTO_TABLE = ShuffleboardTable.getTable("Auto");
     }
 }
