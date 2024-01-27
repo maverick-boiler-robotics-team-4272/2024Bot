9fe27da6d049e59a6dee18f6e68f1d641c3a7d7c
Daniel Quednow <dqued56@tscstudents.net>
2024-01-27 10:47:49 -0500

    Rename elevator subsystem
    Renamed to ArmElevatorSubsystem because it holds both the arm and the
    elevator, and not putting subsystem at the end felt wrong

---
+++ src/main/java/frc/robot/subsystems/armelevator/ArmElevatorSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.armelevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmElevatorSubsystem extends SubsystemBase {
+    
+}
--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.elevator;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Elevator extends SubsystemBase {
-    
-}
