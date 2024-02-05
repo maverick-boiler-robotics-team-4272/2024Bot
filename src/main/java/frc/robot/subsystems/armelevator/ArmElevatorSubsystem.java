5eced2c6dddb3659a98b6fe6d27ea49d8eaace93
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 18:09:51 -0500

    Set up subsystem

--- src/main/java/frc/robot/subsystems/armelevator/ArmElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/armelevator/ArmElevatorSubsystem.java
@@ -5,30 +5,48 @@ import static frc.robot.constants.HardwareMap.*;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.utils.*;
 public class ArmElevatorSubsystem extends SubsystemBase implements Loggable {
     @AutoLog
-    public static class ArmInputs {
-
+    public static class ArmElevatorInputs {
+        public double elevatorHeight;
     }
 
     private Vortex elevatorMotor1; // Might become a NEO, not yet decided by CAD
     private Vortex elevatorMotor2; // Might become a NEO, not yet decided by CAD
     private Vortex armMotor;
 
-    private ArmInputsAutoLogged armInputs;
+    private ArmElevatorInputsAutoLogged armElevatorInputs;
 
     public ArmElevatorSubsystem() {
         elevatorMotor1 = VortexBuilder.createWithDefaults(ELEVATOR_MOTOR_1_ID)
+            .withPosition(0)
+            // .withPositionConversionFactor() with ratio
+            // .withSoftLimits(top, bottom) 
             .build();
         elevatorMotor2 = VortexBuilder.createWithDefaults(ELEVATOR_MOTOR_2_ID)
+            .asFollower(elevatorMotor1, false)
             .build();
         armMotor = VortexBuilder.createWithDefaults(ARM_MOTOR_ID)
             .build();
 
-        armInputs = new ArmInputsAutoLogged();
+        armElevatorInputs = new ArmElevatorInputsAutoLogged();
+    }
+
+    private void setShooterRotation(Rotation2d r) {
+
+    }
+
+    private void setElevatorHeight(double h) {
+
+    }
+
+    public void goToPos(Rotation2d r, double h) {
+        setShooterRotation(r);
+        setElevatorHeight(h);
     }
 
     @Override
@@ -37,7 +55,7 @@ public class ArmElevatorSubsystem extends SubsystemBase implements Loggable {
         elevatorMotor2.log(subdirectory + "/" + humanReadableName, "ElevatorMotor2");
         armMotor.log(subdirectory + "/" + humanReadableName, "ArmMotor");
 
-        Logger.processInputs(subdirectory + "/" + humanReadableName, armInputs);
+        Logger.processInputs(subdirectory + "/" + humanReadableName, armElevatorInputs);
     }
 
     @Override
