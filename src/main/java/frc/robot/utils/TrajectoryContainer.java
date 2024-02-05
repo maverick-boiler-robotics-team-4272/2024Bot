4910c982105c9378bb36bd9badefa3de3d7d76d8
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 18:06:06 -0500

    Add a path not made with pathplanner

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -1,8 +1,11 @@
 package frc.robot.utils;
 
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
@@ -12,10 +15,22 @@ public class TrajectoryContainer {
 
     public final PathPlannerTrajectory TEST_PATH;
     public final PathPlannerTrajectory TUNE_PATH;
+    public final PathPlannerTrajectory STRESS_TEST_PATH;
+    public final PathPlannerTrajectory HOMEMADE_PATH = new PathPlannerTrajectory(
+        new PathPlannerPath(
+            PathPlannerPath.bezierFromPoses(
+                new Pose2d(1.5, 2, new Rotation2d(0)), 
+                new Pose2d(1.5, 5, new Rotation2d(Math.PI / 2))), 
+            new PathConstraints(2, 3, 0.5, 2), 
+            new GoalEndState(0, new Rotation2d(Math.PI / 2))), 
+        INITIAL_SPEEDS, 
+        INITIAL_ROTATION
+    );
 
     public TrajectoryContainer(String prefix) {
         TEST_PATH = PathPlannerPath.fromPathFile(prefix + " Test Path").getTrajectory(INITIAL_SPEEDS, INITIAL_ROTATION);
         TUNE_PATH = PathPlannerPath.fromPathFile(prefix + " Tune Path").getTrajectory(INITIAL_SPEEDS, INITIAL_ROTATION);
+        STRESS_TEST_PATH = PathPlannerPath.fromPathFile(prefix + " Stress Test Path").getTrajectory(INITIAL_SPEEDS, Rotation2d.fromDegrees(-90));
 
     }
 }
