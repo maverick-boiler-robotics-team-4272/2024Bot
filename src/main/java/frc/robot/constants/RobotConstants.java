6528a633991721d15f3fe518df94bfd1ec1d0438
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 18:01:30 -0500

    Retune pid and add speed constants

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -17,6 +17,8 @@ public class RobotConstants {
         }
 
         public static final double WHEEL_DISTANCE = Units.Meters.convertFrom(1, Units.Feet);
+        public static final double MAX_TRANSLATIONAL_SPEED = 2.0;
+        public static final double MAX_ROTATIONAL_SPEED = 2 * Math.PI;
 
         public static final Translation2d FRONT_LEFT_POSITION  = new Translation2d(-WHEEL_DISTANCE,  WHEEL_DISTANCE);
         public static final Translation2d FRONT_RIGHT_POSITION = new Translation2d(-WHEEL_DISTANCE, -WHEEL_DISTANCE);
@@ -34,6 +36,7 @@ public class RobotConstants {
             }
 
             public static final double WHEEL_RADIUS = 2.0; // Inches
+            public static final double MAX_MODULE_SPEED = Units.MetersPerSecond.convertFrom(14.5, Units.FeetPerSecond);
 
             public static final double DRIVE_RATIO = 6.75 / 1.0;
             public static final double STEER_RATIO = 150.0 / 7.0;
@@ -41,7 +44,7 @@ public class RobotConstants {
             public static final double DRIVE_PID_P = 0.003596;
             public static final double DRIVE_PID_I = 0.0;
             public static final double DRIVE_PID_D = 0.0;
-            public static final double DRIVE_PID_F = 0.6;
+            public static final double DRIVE_PID_F = 0.245;
 
             public static final double STEER_PID_P = 0.01;
             public static final double STEER_PID_I = 0.0001;
