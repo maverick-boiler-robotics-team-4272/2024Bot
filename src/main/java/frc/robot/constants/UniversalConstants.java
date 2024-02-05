05b5eb4afcda72e05305eec3c53fe3b0b27ddc34
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 18:05:39 -0500

    Add positions for speaker and amp
    Only for blue side

--- src/main/java/frc/robot/constants/UniversalConstants.java
+++ src/main/java/frc/robot/constants/UniversalConstants.java
@@ -1,5 +1,8 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
 
 public class UniversalConstants {
@@ -9,6 +12,10 @@ public class UniversalConstants {
 
     public static final double FIELD_WIDTH_METERS = Units.Meters.convertFrom(54, Units.Feet) + Units.Meters.convertFrom(3.25, Units.Inches);
     public static final double FIELD_HEIGHT_METERS = Units.Meters.convertFrom(26, Units.Feet) + Units.Meters.convertFrom(11.25, Units.Inches);
+
+    public static final Translation2d SPEAKER_POSITION = new Translation2d(0, Units.Meters.convertFrom(218.42, Units.Inches));
+
+    public static final Pose2d AMP_POSE = new Pose2d(Units.Meters.convertFrom(72.5, Units.Inches), Units.Meters.convertFrom(300, Units.Inches), new Rotation2d(Math.PI / 2));
 
     public static final double FIELD_HALF_WIDTH_METERS = FIELD_WIDTH_METERS / 2.0;
     public static final double FIELD_HALF_HEIGHT_METERS = FIELD_HEIGHT_METERS / 2.0;
