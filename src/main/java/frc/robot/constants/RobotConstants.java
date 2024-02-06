723eb4701f8d3e068f00ada44941a4cbb679c3c5
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 19:58:52 -0500

    Add Arm and Elevator Setpoints and constants

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
 
@@ -53,11 +54,55 @@ public class RobotConstants {
         }
     }
 
+    public static interface ArmElevatorSetpoint {
+        public double getElevatorHeight();
+        public Rotation2d getArmAngle();
+
+        public static ArmElevatorSetpoint createArbitrarySetpoint(double elevatorHeight, Rotation2d armAngle) {
+            return new ArmElevatorSetpoint() {
+                @Override
+                public double getElevatorHeight() {
+                    return elevatorHeight;
+                }
+
+                @Override
+                public Rotation2d getArmAngle() {
+                    return armAngle;
+                }
+            };
+        }
+    }
+
+    public static enum ArmElevatorSetpoints implements ArmElevatorSetpoint {
+        ZERO(new Rotation2d(0), 0);
+
+        private Rotation2d armAngle;
+        private double elevatorHeight;
+
+        private ArmElevatorSetpoints(Rotation2d armAngle, double elevatorHeight) {
+            this.armAngle = armAngle;
+            this.elevatorHeight = elevatorHeight;
+        }
+
+        @Override
+        public Rotation2d getArmAngle() {
+            return armAngle;
+        }
+
+        @Override
+        public double getElevatorHeight() {
+            return elevatorHeight;
+        }
+    }
+
     public static class ArmConstants {
         public static final double ARM_PID_P = 0.001;
         public static final double ARM_PID_I = 0.0001;
         public static final double ARM_PID_D = 0.0;
         public static final double ARM_PID_F = 0.0;
+
+        public static final Rotation2d MAX_ARM_ANGLE = Rotation2d.fromDegrees(50.0);
+        public static final Rotation2d MIN_ARM_ANGLE = Rotation2d.fromDegrees(-20.0);
     }
 
     public static class ElevatorConstants {
@@ -65,5 +110,9 @@ public class RobotConstants {
         public static final double ELEVATOR_PID_I = 0.0001;
         public static final double ELEVATOR_PID_D = 0.0;
         public static final double ELEVATOR_PID_F = 0.0;
+
+        //TODO: find out from CAD
+        public static final double MAX_ELEVATOR_HEIGHT = Units.Meters.convertFrom(48.0, Units.Inches);
+        public static final double MIN_ELEVATOR_HEIGHT = Units.Meters.convertFrom(0, Units.Inches);
     }
 }
