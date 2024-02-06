ba21560a9bfef0cccc592c79aa824cb8e869154c
Daniel Quednow <dqued56@tscstudents.net>
2024-02-05 19:58:18 -0500

    Fix Initial Heading

--- src/main/java/frc/robot/utils/TrajectoryBuilder.java
+++ src/main/java/frc/robot/utils/TrajectoryBuilder.java
@@ -9,15 +9,16 @@ import com.pathplanner.lib.path.PathPlannerTrajectory;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class TrajectoryBuilder {
     public static PathPlannerPath buildPath(Pose2d start, Pose2d end) {
         PathPlannerPath path = new PathPlannerPath(
-            PathPlannerPath.bezierFromPoses(start, end), 
+            PathPlannerPath.bezierFromPoses(new Pose2d(start.getTranslation(), start.minus(end).getRotation()), end), 
             new PathConstraints(
                 MAX_TRANSLATIONAL_SPEED, 
-                9.81, //gravity but sidways
+                3 * 9.81, //3g's but sidways
                 MAX_ROTATIONAL_SPEED, 
                 Rotation2d.fromDegrees(720).getRadians()
             ), 
@@ -30,7 +31,7 @@ public class TrajectoryBuilder {
     public static PathPlannerTrajectory goToPosition(Drivetrain drivetrain, Pose2d position) {
         PathPlannerTrajectory trajectory = new PathPlannerTrajectory(
             buildPath(drivetrain.getRobotPose(), position),
-            drivetrain.getChassisSpeeds(), 
+            ChassisSpeeds.fromRobotRelativeSpeeds(drivetrain.getChassisSpeeds(), drivetrain.getRobotPose().getRotation()), 
             drivetrain.getRobotPose().getRotation()
         );
 
