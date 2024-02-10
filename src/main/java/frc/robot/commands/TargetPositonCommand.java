40f901eb8267fd9a359d02822b4fb32f61616d92
alohr86 <lohrandrew@icloud.com>
2024-02-10 12:17:09 -0500

    Make a target position command

---
+++ src/main/java/frc/robot/commands/TargetPositonCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+public class TargetPositonCommand extends Command {
+    Drivetrain drivetrain;
+    ArmElevatorSubsystem armElevator;
+    Translation3d target;
+
+    DoubleSupplier xSpeed;
+    DoubleSupplier ySpeed;
+
+    public TargetPositonCommand(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, Translation3d target, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
+        this.drivetrain = drivetrain;
+        this.armElevator = armElevator;
+        this.target = target;
+
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+
+        addRequirements(drivetrain, armElevator);
+    }
+
+    @Override
+    public void execute() {
+        armElevator.targetPos(drivetrain.getRobotPose().getTranslation(), target);
+    }
+}
