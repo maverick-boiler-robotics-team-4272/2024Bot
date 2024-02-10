f9a9d27ace3330a5f095255dbf0525d64244c8a4
alohr86 <lohrandrew@icloud.com>
2024-02-10 15:28:10 -0500

    Fix Auto aim command subsystem requirments.

--- src/main/java/frc/robot/commands/AutoAimCommand.java
+++ src/main/java/frc/robot/commands/AutoAimCommand.java
@@ -6,13 +6,14 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
+import frc.robot.subsystems.armelevator.states.TargetPositionState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.states.FacePositionState;
 
 public class AutoAimCommand extends ParallelCommandGroup {
     public AutoAimCommand(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
         super(
-            new TargetPositonCommand(drivetrain, armElevator, SPEAKER_SHOT_POSITION),
+            new TargetPositionState(armElevator, () -> drivetrain.getRobotPose().getTranslation(), SPEAKER_SHOT_POSITION),
             new FacePositionState(drivetrain, xSpeed, ySpeed, SPEAKER_SHOT_POSITION.toTranslation2d())
         );
     }
