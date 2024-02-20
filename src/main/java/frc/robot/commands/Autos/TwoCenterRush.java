c52890a0c388463403097dd433322ac9a2dfbe44
Andy Lohr <lohrandrew@icloud.com>
2024-02-19 20:04:59 -0500

    Create two center rush

---
+++ src/main/java/frc/robot/commands/Autos/TwoCenterRush.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.autos;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+public class TwoCenterRush extends SequentialCommandGroup {
+    public TwoCenterRush(Drivetrain drivetrain) {
+        super(
+            
+        );
+    }
+}
