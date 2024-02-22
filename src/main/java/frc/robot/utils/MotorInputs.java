ff30bff6ae93bd237608bfbeedb982af5df55c50
Daniel Quednow <dqued56@tscstudents.net>
2024-02-22 18:34:17 -0500

    Pull all motor inputs to an external class
    Pulled motor inputs to an external class to be used in both NEO and
    Vortex
    Added extra things to log

---
+++ src/main/java/frc/robot/utils/MotorInputs.java
@@ -0,0 +1,18 @@
+package frc.robot.utils;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class MotorInputs {
+    public double motorVoltage;
+    public double motorTemperatureCelsius;
+    public double outputCurrent;
+    public boolean stalling;
+
+    public double currentLimit;
+    public double freeLimit;
+    public double limitRPM;
+
+    public double motorVelocity;
+    public double motorPosition;
+}
