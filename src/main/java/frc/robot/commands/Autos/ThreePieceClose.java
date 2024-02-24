badf229d0fdc6e511e195f8622eaee627e1347c1
Andy Lohr <lohrandrew@icloud.com>
2024-02-24 14:41:20 -0500

    Replace with path follow aim commands

--- src/main/java/frc/robot/commands/Autos/ThreePieceClose.java
+++ src/main/java/frc/robot/commands/Autos/ThreePieceClose.java
@@ -3,13 +3,14 @@ package frc.robot.commands.autos;
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.PathFollowWithAimCommand;
+import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.subsystems.drivetrain.states.PathFollowState;
 
 public class ThreePieceClose extends SequentialCommandGroup {
-    public ThreePieceClose(Drivetrain drivetrain) {
+    public ThreePieceClose(Drivetrain drivetrain, ArmElevatorSubsystem armElevator) {
         super(
-            new PathFollowState(drivetrain, getGlobalTrajectories().THREE_PIECE_CLOSE, true, false)
+            new PathFollowWithAimCommand(drivetrain, armElevator, getGlobalTrajectories().THREE_PIECE_CLOSE)
         );
     }
 }
--- src/main/java/frc/robot/commands/Autos/TwoCenterRush.java
+++ src/main/java/frc/robot/commands/Autos/TwoCenterRush.java
@@ -1,15 +1,15 @@
 package frc.robot.commands.autos;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.PathFollowWithAimCommand;
+import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.subsystems.drivetrain.states.PathFollowState;
 
 public class TwoCenterRush extends SequentialCommandGroup {
-    public TwoCenterRush(Drivetrain drivetrain) {
+    public TwoCenterRush(Drivetrain drivetrain, ArmElevatorSubsystem armElevator) {
         super(
-            new PathFollowState(drivetrain, getGlobalTrajectories().TWO_CENTER_RUSH)
+            new PathFollowWithAimCommand(drivetrain, armElevator, getGlobalTrajectories().TWO_CENTER_RUSH)
         );
     }
 }
