6862029788f5c7b0c55e809bf2e9b3b2a0e452c6
Andy Lohr <lohrandrew@icloud.com>
2024-02-27 08:58:59 -0500

    Disable and enable vision fusion in auto

--- src/main/java/frc/robot/commands/autos/TwoCenterRush.java
+++ src/main/java/frc/robot/commands/autos/TwoCenterRush.java
@@ -2,15 +2,19 @@ package frc.robot.commands.autos;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.PathFollowWithAimCommand;
+import frc.robot.commands.PathFollowWithEvents;
 import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class TwoCenterRush extends SequentialCommandGroup {
     public TwoCenterRush(Drivetrain drivetrain, ArmElevatorSubsystem armElevator) {
         super(
-            new PathFollowWithAimCommand(drivetrain, armElevator, getGlobalTrajectories().TWO_CENTER_RUSH.trajectory)
+            new InstantCommand(drivetrain::disableVisionFusion),
+            new PathFollowWithEvents(new PathFollowWithAimCommand(drivetrain, armElevator, getGlobalTrajectories().TWO_CENTER_RUSH.trajectory), getGlobalTrajectories().TWO_CENTER_RUSH),
+            new InstantCommand(drivetrain::enableVisionFusion)
         );
     }
 }
