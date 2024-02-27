5e0faca7218e90be8abb2639d7fcb5486f699368
Andy Lohr <lohrandrew@icloud.com>
2024-02-27 14:06:12 -0500

    Add lidar and april tag to auto

--- src/main/java/frc/robot/subsystems/armelevator/states/TargetPositionState.java
+++ src/main/java/frc/robot/subsystems/armelevator/states/TargetPositionState.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.armelevator.states;
 
 
+import static frc.robot.constants.RobotConstants.ArmElevatorSetpoints.HOME;
+
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Translation2d;
@@ -12,20 +15,30 @@ import frc.team4272.globals.State;
 public class TargetPositionState extends State<ArmElevatorSubsystem> {
     Supplier<Translation2d> drivetrainPos;
     Translation3d target;
+    BooleanSupplier lidar;
 
-    public TargetPositionState(ArmElevatorSubsystem armElevator, Supplier<Translation2d> driveTrainPos, Translation3d target) {
+    public TargetPositionState(ArmElevatorSubsystem armElevator, Supplier<Translation2d> driveTrainPos, Translation3d target, BooleanSupplier lidar) {
         super(armElevator);
         this.target = target;
         this.drivetrainPos = driveTrainPos;
+        this.lidar = lidar;
+    }
+
+    public TargetPositionState(ArmElevatorSubsystem armElevator, Supplier<Translation2d> driveTrainPos, Translation3d target) {
+        this(armElevator, driveTrainPos, target, () -> true);
     }
 
     @Override
     public void execute() {
-        requiredSubsystem.targetPos(drivetrainPos.get(), target);
+        if(lidar.getAsBoolean()) {
+            requiredSubsystem.targetPos(drivetrainPos.get(), target);
+        } else {
+            requiredSubsystem.goToPos(HOME);
+        }
     }
 
     @Override
     public void end(boolean interrupted) {
-        requiredSubsystem.goToPos(ArmElevatorSetpoints.HOME);
+        requiredSubsystem.goToPos(HOME);
     }
 }
