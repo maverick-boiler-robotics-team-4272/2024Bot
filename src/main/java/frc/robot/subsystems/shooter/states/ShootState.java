86279db1820a4dcc60490072b7cbb6de930393de
Andy Lohr <lohrandrew@icloud.com>
2024-02-28 11:18:22 -0500

    Add lidar to shootstate to stop when there is no note

--- src/main/java/frc/robot/subsystems/shooter/states/ShootState.java
+++ src/main/java/frc/robot/subsystems/shooter/states/ShootState.java
@@ -6,18 +6,29 @@ import frc.team4272.globals.State;
 public class ShootState extends State<Shooter> {
     private double shootPower;
     private double feedPower;
+    private boolean useLidar;
     
-    public ShootState(Shooter shooter, double shootPower, double feedPower) {
+    public ShootState(Shooter shooter, double shootPower, double feedPower, boolean useLidar) {
         super(shooter);
 
         this.shootPower = shootPower;
         this.feedPower = feedPower;
+        this.useLidar = useLidar;
+    }
+
+    public ShootState(Shooter shooter, double shootPower, double feedPower) {
+        this(shooter, shootPower, feedPower, false);
     }
 
     @Override
     public void initialize() {
         requiredSubsystem.rev(shootPower);
         requiredSubsystem.feed(feedPower);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return useLidar && !requiredSubsystem.lidarTripped();
     }
 
     @Override
