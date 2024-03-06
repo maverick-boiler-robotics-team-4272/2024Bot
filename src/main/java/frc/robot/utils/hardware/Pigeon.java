7d83ecb9131a191b9d9ef2d00485904c591fb805
Daniel Quednow <dqued56@tscstudents.net>
2024-03-06 18:42:40 -0500

    Add accelerometer to pigeon logging
    Added accelerometer readouts to pigeon logging
    Added X,Y,Z and XY angle/magnitude

--- src/main/java/frc/robot/utils/hardware/Pigeon.java
+++ src/main/java/frc/robot/utils/hardware/Pigeon.java
@@ -18,6 +18,13 @@ public class Pigeon extends Pigeon2 implements Gyroscope, Loggable {
     public static class PigeonInputs {
         public double gyroscopeAngleDegrees;
         public double outputAngleDegrees;
+
+        public double xAcceleration;
+        public double yAcceleration;
+        public double zAcceleration;
+
+        public Rotation2d xyAccelerationAngle;
+        public double xyAccelerationMagnitude;
     }
 
     private PigeonInputsAutoLogged pigeonInputs;
@@ -45,6 +52,12 @@ public class Pigeon extends Pigeon2 implements Gyroscope, Loggable {
     public void log(String subdirectory, String humanReadableName) {
         pigeonInputs.gyroscopeAngleDegrees = getYaw().getValueAsDouble();
         pigeonInputs.outputAngleDegrees = -getYaw().getValueAsDouble() + offset;
+        pigeonInputs.xAcceleration = getAccelerationX().getValueAsDouble();
+        pigeonInputs.yAcceleration = getAccelerationY().getValueAsDouble();
+        pigeonInputs.zAcceleration = getAccelerationZ().getValueAsDouble();
+
+        pigeonInputs.xyAccelerationAngle = new Rotation2d(pigeonInputs.yAcceleration, pigeonInputs.xAcceleration);
+        pigeonInputs.xyAccelerationMagnitude = Math.hypot(pigeonInputs.yAcceleration, pigeonInputs.xAcceleration);
 
         Logger.processInputs(subdirectory + "/" + humanReadableName, pigeonInputs);
     }
