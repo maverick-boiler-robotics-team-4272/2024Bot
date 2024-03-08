3a9adecb363737cbc7bd2dd7f382d052ad56c1ba
Daniel Quednow <dqued56@tscstudents.net>
2024-03-08 08:33:01 -0500

    Slow outfeeding

--- src/main/java/frc/robot/commands/IntakeFeedCommand.java
+++ src/main/java/frc/robot/commands/IntakeFeedCommand.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.IntakeState;
 import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.states.FeedState;
 import frc.robot.subsystems.shooter.states.LidarStoppedFeedState;
 import frc.robot.subsystems.shooter.states.LidarStoppedOutfeedState;
 
@@ -19,7 +18,7 @@ public class IntakeFeedCommand extends SequentialCommandGroup {
                 new IntakeState(intake, power),
                 new LidarStoppedFeedState(shooter, power)
             ),
-            new LidarStoppedOutfeedState(shooter, () -> 0.3)
+            new LidarStoppedOutfeedState(shooter, () -> 0.1)
         );
     }
 }
