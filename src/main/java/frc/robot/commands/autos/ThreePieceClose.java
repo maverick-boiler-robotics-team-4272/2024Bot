916cca51dcf579ebb1cdc7034ff53b1fdaf29fb6
Andy Lohr <lohrandrew@icloud.com>
2024-03-09 09:51:11 -0500

    Add pause events to ThreePieceClose

--- src/main/java/frc/robot/commands/autos/ThreePieceClose.java
+++ src/main/java/frc/robot/commands/autos/ThreePieceClose.java
@@ -8,6 +8,7 @@ import frc.robot.subsystems.shooter.states.*;
 // Subsystems
 import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.states.PathFollowState;
 import frc.robot.subsystems.shooter.Shooter;
 
 // Constants
@@ -15,114 +16,17 @@ import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
 public class ThreePieceClose extends SequentialCommandGroup {
     public ThreePieceClose(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, Shooter shooter) {
-        PathFollowWithAimCommand aim = new PathFollowWithAimCommand(
-            drivetrain, 
-            armElevator, 
-            getGlobalTrajectories().THREE_PIECE_CLOSE.trajectory
-        );
-
-        PathFollowWithEvents events = new PathFollowWithEvents(aim, getGlobalTrajectories().THREE_PIECE_CLOSE);
-
-        events.addPauseTime(0.4, new SequentialCommandGroup(
-            new InstantCommand(aim::pausePathFollowing),
-            new ParallelRaceGroup(
-                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
-                new RevState(shooter, 1.0).withTimeout(0.75).andThen(
-                    new InstantCommand(drivetrain::enableVisionFusion),
-                    new AutoShootState(shooter, 1.0, 1.0)
-                )
-            ),
-            new InstantCommand(() -> {
-                drivetrain.disableVisionFusion();
-                aim.unpausePathFollowing();
-                events.unpause();
-            })
-        ));
-        
-        events.addPauseTime(2.5, new SequentialCommandGroup(
-            new InstantCommand(() -> {
-                aim.pausePathFollowing();
-                drivetrain.enableVisionFusion();
-            }),
-            new ParallelRaceGroup(
-                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
-                new LidarStoppedFeedState(shooter, 0.8).andThen(
-                    new OutfeedState(shooter, 0.2).withTimeout(0.2),
-                    new WaitCommand(0.1),
-                    new AutoShootState(shooter, 1.0, 1.0).withTimeout(5)
-                )
-            ),
-            new InstantCommand(() -> {
-                drivetrain.disableVisionFusion();
-                aim.unpausePathFollowing();
-                events.unpause();
-            })
-        ));
-
-        events.addPauseTime(4.5, new SequentialCommandGroup(
-            new InstantCommand(() -> {
-                aim.pausePathFollowing();
-                drivetrain.enableVisionFusion();
-            }),
-            new ParallelRaceGroup(
-                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
-                new LidarStoppedFeedState(shooter, 0.8).andThen(
-                    new OutfeedState(shooter, 0.2).withTimeout(0.2),
-                    new WaitCommand(0.1),
-                    new AutoShootState(shooter, 1.0, 1.0)
-                )
-            ),
-            new InstantCommand(() -> {
-                drivetrain.disableVisionFusion();
-                aim.unpausePathFollowing();
-                events.unpause();
-            })
-        ));
-
-        events.addPauseTime(5.2, new SequentialCommandGroup(
-            new InstantCommand(() -> {
-                aim.pausePathFollowing();
-                drivetrain.enableVisionFusion();
-            }),
-            new ParallelRaceGroup(
-                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
-                new LidarStoppedFeedState(shooter, 0.8).andThen(
-                    new OutfeedState(shooter, 0.2).withTimeout(0.2),
-                    new WaitCommand(0.1),
-                    new AutoShootState(shooter, 1.0, 1.0)
-                )
-            ),
-            new InstantCommand(() -> {
-                drivetrain.disableVisionFusion();
-                aim.unpausePathFollowing();
-                events.unpause();
-            })
-        ));
-
-        events.addPauseTime(9.0, new SequentialCommandGroup(
-            new InstantCommand(() -> {
-                aim.pausePathFollowing();
-                drivetrain.enableVisionFusion();
-            }),
-            new ParallelRaceGroup(
-                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
-                new LidarStoppedFeedState(shooter, 0.8).andThen(
-                    new OutfeedState(shooter, 0.2).withTimeout(0.2),
-                    new WaitCommand(0.1),
-                    new AutoShootState(shooter, 1.0, 1.0)
-                )
-            ),
-            new InstantCommand(() -> {
-                drivetrain.disableVisionFusion();
-                aim.unpausePathFollowing();
-                events.unpause();
-            })
-        ));
-
-        addCommands(
+        super(
             new InstantCommand(drivetrain::disableVisionFusion),
-            new InstantCommand(drivetrain::resetModules),
-            events
+            new PathFollowWithEvents(
+                new PathFollowState(
+                    drivetrain, 
+                    getGlobalTrajectories().THREE_PIECE_CLOSE.trajectory,
+                    true,
+                    false
+                ), 
+                getGlobalTrajectories().THREE_PIECE_CLOSE
+            )
         );
     }
 }
