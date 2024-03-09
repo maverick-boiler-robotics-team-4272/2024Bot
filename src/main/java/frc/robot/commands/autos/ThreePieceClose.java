e5a54e77ea02e949678e843c61457469aa2d0a48
Andy Lohr <lohrandrew@icloud.com>
2024-03-09 13:48:11 -0500

    Tune Three piece close

--- src/main/java/frc/robot/commands/autos/ThreePieceClose.java
+++ src/main/java/frc/robot/commands/autos/ThreePieceClose.java
@@ -7,25 +7,37 @@ import frc.robot.subsystems.shooter.states.*;
 
 // Subsystems
 import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
+import frc.robot.subsystems.armelevator.states.GoToArmElevatorState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
 import frc.robot.subsystems.shooter.Shooter;
 
+import static frc.robot.constants.AutoConstants.PathFollowConstants.DEFAULT_POSE_DELTA;
 // Constants
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
+import static frc.robot.constants.RobotConstants.ArmElevatorSetpoints.HOME;
 
 public class ThreePieceClose extends SequentialCommandGroup {
     public ThreePieceClose(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, Shooter shooter) {
         super(
             new InstantCommand(drivetrain::disableVisionFusion),
+            new InstantCommand(drivetrain::resetModules),
+            new GoToArmElevatorState(armElevator, HOME),
             new PathFollowWithEvents(
                 new PathFollowState(
                     drivetrain, 
                     getGlobalTrajectories().THREE_PIECE_CLOSE,
                     true,
                     false
-                ), 
+                ).withEndConditions(
+                    true, 
+                    false, 
+                    DEFAULT_POSE_DELTA), 
                 getGlobalTrajectories().THREE_PIECE_CLOSE
+            ),
+            new ParallelRaceGroup(
+                new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
+                new AutoShootState(shooter, 1.0, 1.0)
             )
         );
     }
