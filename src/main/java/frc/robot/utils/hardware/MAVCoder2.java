409448c7f68bb690288cc4ded45a22f7337d005d
Daniel Quednow <dqued56@tscstudents.net>
2024-03-09 08:32:33 -0500

    Change MAVCoder math to be done by the spark
    Changed the mavcoder math (offsetting) to be done by the built-in
    functionality with the Spark that we have the encoder plugged into

--- src/main/java/frc/robot/utils/hardware/MAVCoder2.java
+++ src/main/java/frc/robot/utils/hardware/MAVCoder2.java
@@ -14,35 +14,27 @@ public class MAVCoder2 implements Loggable {
     @AutoLog
     public static class MAVCoder2Inputs {
         public double readAngleDegrees;
-        public double offsetAngleDegrees;
     }
 
     private SparkAbsoluteEncoder encoder;
-    private double offset;
 
     private MAVCoder2InputsAutoLogged inputs;
 
     public MAVCoder2(CANSparkBase motor, double offset) {
         this.encoder = motor.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
-        this.offset = offset;
         this.inputs = new MAVCoder2InputsAutoLogged();
 
-        this.encoder.setZeroOffset(MAV_2_MIN_OUTPUT);
         this.encoder.setPositionConversionFactor(MAV_2_POSITION_FACTOR);
-    }
-
-    public double getUnoffsetPosition() {
-        return encoder.getPosition();
+        this.encoder.setZeroOffset(MAV_2_ANGLE_OFFSET + offset);
     }
 
     public double getPosition() {
-        return encoder.getPosition() - offset;
+        return encoder.getPosition();
     }
 
     @Override
     public void log(String subdirectory, String humanReadableName) {
-        inputs.readAngleDegrees = getUnoffsetPosition();
-        inputs.offsetAngleDegrees = inputs.readAngleDegrees - offset;
+        inputs.readAngleDegrees = getPosition();
 
         Logger.processInputs(subdirectory + "/" + humanReadableName, inputs);
     }
