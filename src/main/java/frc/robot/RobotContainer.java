5cac83ab72d8897e141d35ddb2a436ddee4e4ab5
Andy Lohr <lohrandrew@icloud.com>
2024-03-11 15:00:35 -0400

    Change AMP to higher top spin to push note down

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -182,7 +182,7 @@ public class RobotContainer {
         JoystickPOV operatorDPad = operatorController.getPOV("d-pad");
 
         new Trigger(operatorController.getButton("a")::get).whileTrue(
-            new ImbalancedShootState(shooter, 0.25, 0.05, 0.2)
+            new ImbalancedShootState(shooter, 0.50, 0.05, 0.2)
         );
 
         new Trigger(operatorController.getButton("y")::get).onTrue(
