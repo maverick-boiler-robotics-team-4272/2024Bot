36503847268686852f0ad4fb17be7af4932431f0
Daniel Quednow <dqued56@tscstudents.net>
2024-03-21 18:09:36 -0400

    Add stall code to intake state
    Added comments to explain what stuff does

--- src/main/java/frc/robot/subsystems/intake/states/IntakeState.java
+++ src/main/java/frc/robot/subsystems/intake/states/IntakeState.java
@@ -6,6 +6,9 @@ import frc.team4272.globals.State;
 public class IntakeState extends State<IntakeSubsystem> {
     private double intakePower;
 
+    private int outtakeTime;
+    private boolean needsUpdate;
+
     public IntakeState(IntakeSubsystem intake, double power) {
         super(intake);
         this.intakePower = power;
@@ -13,7 +16,40 @@ public class IntakeState extends State<IntakeSubsystem> {
 
     @Override
     public void initialize() {
-        requiredSubsystem.runMotor(intakePower);
+        outtakeTime = -1;
+        needsUpdate = true;
+    }
+
+    @Override
+    public void execute() {
+        // If outtake time is not the "Dont outtake" number
+        if(outtakeTime != -1) {
+            // If we need to change the power in the motor
+            if(needsUpdate) {
+                requiredSubsystem.runMotor(-intakePower);
+                needsUpdate = false;
+            }
+
+            // If we have been outtaking for half a second
+            if(outtakeTime >= 25) {
+                outtakeTime = -1;
+                needsUpdate = true;
+            }
+
+            outtakeTime++;
+        } else {
+            // If we need to change the power in the motor
+            if(needsUpdate) {
+                requiredSubsystem.runMotor(intakePower);
+                needsUpdate = false;
+            }
+
+            // If the motor is stalling, tell it to outtake
+            if(requiredSubsystem.isMotorStalling()) {
+                outtakeTime = 0;
+                needsUpdate = true;
+            }
+        }
     }
 
     @Override
