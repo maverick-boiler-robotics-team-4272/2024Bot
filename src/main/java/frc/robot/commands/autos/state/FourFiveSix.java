db2ab28d878882c66b2df29011ba7aca6c6deef1
Andy Lohr <lohrandrew@icloud.com>
2024-04-01 17:52:44 -0400

    Add state path commands

---
+++ src/main/java/frc/robot/commands/autos/state/FourFiveSix.java
@@ -0,0 +1,30 @@
+package frc.robot.commands.autos.state;
+
+import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
+import static frc.robot.constants.RobotConstants.ArmElevatorSetpoints.SUB_SHOT;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.PathFollowWithEvents;
+import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
+import frc.robot.subsystems.armelevator.states.GoToArmElevatorState;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.states.PathFollowState;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.states.AutoShootState;
+
+public class FourFiveSix extends SequentialCommandGroup {
+    public FourFiveSix(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, Shooter shooter) {
+        super(
+            new GoToArmElevatorState(armElevator, SUB_SHOT).deadlineWith(
+                new AutoShootState(shooter, 1.0, 1.0)
+            ),
+            new PathFollowWithEvents(
+                new PathFollowState(
+                    drivetrain, 
+                    getGlobalTrajectories().P_456
+                ), 
+                getGlobalTrajectories().P_456
+            )
+        );
+    }
+}
