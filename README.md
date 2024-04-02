83266188c07e4928cadcf73c7ed71f59e069515f
Dannie <danquednow@gmail.com>
2024-04-02 14:59:35 -0400

    Increase the BEAN variety

--- README.md
+++ README.md
@@ -3,7 +3,30 @@ Robot code for Crecendo
 
 ## Jokes
 
-### Unit: The BEAN
+### Unit: The Best Empirically Allowed Number (BEAN)
 
 A unit of measurement for a percentage output. 1 BEAN is 100%
-BEAN stands for: Best Empirical Allowed Number
+
+#### Modifiers:
+and: makes a new BEAN with the sum of the current bean and the passed in bean
+without: makes a new BEAN with the difference of the current bean and the passed in bean
+
+stalk: Makes a new BEAN with the same amount in the opposite direction
+refried: Makes a new BEAN with half the current beans
+dip: Makes a new BEAN with double the current beans
+can: Makes a new BEAN with triple the current beans
+basket: Makes a new BEAN with quintuple the current beans
+
+#### Built in BEANS:
+Jelly Bean: 0%
+String Bean: 5%
+Lima Bean: 10%
+Black Bean: 25%
+Cannellini Bean: 40%
+Pinto Bean: 50%
+French Cut Bean: 60%
+Kidney Bean: 75%
+Garbanzo Bean: 90%
+Coffee Bean: 95%
+Baked Beans: 100%
+Bean Soup: 500%
--- src/main/java/frc/robot/utils/misc/BEAN.java
+++ src/main/java/frc/robot/utils/misc/BEAN.java
@@ -7,20 +7,47 @@ public class BEAN {
         this.beans = amountOfBeans;
     }
 
-    public BEAN add(BEAN bean) {
+    public BEAN and(BEAN bean) {
         return new BEAN(beans + bean.beans);
     }
 
-    public BEAN sub(BEAN bean) {
+    public BEAN without(BEAN bean) {
         return new BEAN(beans - bean.beans);
     }
 
+    public BEAN stalk() {
+        return new BEAN(-beans);
+    }
+
+    public BEAN refried() {
+        return new BEAN(beans * 0.5);
+    }
+
+    public BEAN dip() {
+        return new BEAN(beans * 2.0);
+    }
+
+    public BEAN can() {
+        return new BEAN(beans * 3.0);
+    }
+
+    public BEAN basket() {
+        return new BEAN(beans * 5.0);
+    }
+
     public static class Beans {
+        public static final BEAN JELLY_BEAN = new BEAN(0.0);
         public static final BEAN STRING_BEAN = new BEAN(0.05);
-        public static final BEAN LIMA = new BEAN(0.1);
-        public static final BEAN PINTO = new BEAN(0.5);
-        public static final BEAN FRENCH_CUT = new BEAN(0.6);
+        public static final BEAN LIMA_BEAN = new BEAN(0.1);
+        public static final BEAN BLACK_BEAN = new BEAN(0.25);
+        public static final BEAN CANNELLINI_BEAN = new BEAN(0.4);
+        public static final BEAN PINTO_BEAN = new BEAN(0.5);
+        public static final BEAN FRENCH_CUT_BEAN = new BEAN(0.6);
+        public static final BEAN KIDNEY_BEAN = new BEAN(0.75);
+        public static final BEAN GARBANZO_BEAN = new BEAN(0.9);
+        public static final BEAN COFFEE_BEAN = new BEAN(0.95);
         public static final BEAN BAKED_BEAN = new BEAN(1.0);
+        public static final BEAN BEAN_SOUP = new BEAN(5.0);
     }
 
 }
