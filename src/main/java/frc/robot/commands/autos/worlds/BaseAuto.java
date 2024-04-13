d8e403be4173cc6e7ae45c953b7bfb5976318439
Andy Lohr <lohrandrew@icloud.com>
2024-04-13 10:39:00 -0400

    Add a first and last shot command for fun

--- src/main/java/frc/robot/commands/autos/worlds/BaseAuto.java
+++ src/main/java/frc/robot/commands/autos/worlds/BaseAuto.java
@@ -1,16 +1,30 @@
 package frc.robot.commands.autos.worlds;
 
+import static frc.robot.constants.RobotConstants.ArmElevatorSetpoints.START_LINE;
+import static frc.robot.constants.UniversalConstants.getGlobalPositions;
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.AutoAimCommand;
 import frc.robot.commands.PathFollowWithEvents;
+import frc.robot.subsystems.armelevator.ArmElevatorSubsystem;
+import frc.robot.subsystems.armelevator.states.GoToArmElevatorState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.states.FacePositionState;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.states.AutoShootState;
 import frc.robot.utils.paths.Path;
 
 public class BaseAuto extends SequentialCommandGroup {
     protected final PathFollowWithEvents pathFollowCommand;
+    protected final ParallelDeadlineGroup startShot;
+    protected final ParallelRaceGroup endShot;
 
-    public BaseAuto(Drivetrain drivetrain, boolean disable, Path path) {
+    public BaseAuto(Drivetrain drivetrain, ArmElevatorSubsystem armElevator, Shooter shooter, boolean disable, Path path) {
         super(
             new InstantCommand(() -> {
                 drivetrain.resetModules();
@@ -22,6 +36,17 @@ public class BaseAuto extends SequentialCommandGroup {
         pathFollowCommand = new PathFollowWithEvents(
             new PathFollowState(drivetrain, path, true, false),
             path
+        );
+
+        startShot = new ParallelDeadlineGroup(
+            new FacePositionState(drivetrain, () -> 0, () -> 0, getGlobalPositions().SPEAKER_TARGET_POSITION.toTranslation2d()), 
+            new GoToArmElevatorState(armElevator, START_LINE),
+            new AutoShootState(shooter, 1.0, 1.0)
+        );
+
+        endShot = new ParallelRaceGroup(
+            new AutoAimCommand(drivetrain, armElevator, () -> 0, () -> 0),
+            new AutoShootState(shooter, 1.0, 1.0).beforeStarting(new WaitCommand(0.1))
         );
     }
 }
