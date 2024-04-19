4402f473851349ed5a414dabdd4f18420e94d2a0
Andy Lohr <lohrandrew@icloud.com>
2024-04-18 21:26:23 -0500

    Offset shot position because its inset in the speaker

--- src/main/java/frc/robot/utils/misc/PositionsContainer.java
+++ src/main/java/frc/robot/utils/misc/PositionsContainer.java
@@ -20,7 +20,7 @@ public class PositionsContainer {
 
         SPEAKER_POSITION = new Translation2d(red ? FIELD_WIDTH_METERS : 0, Meters.convertFrom(218.42, Inches));
         SPEAKER_SHOT_POSITION = new Translation3d(red ? FIELD_WIDTH_METERS - Meters.convertFrom(10, Inches) : Meters.convertFrom(10, Inches), Meters.convertFrom(218.42, Inches), Meters.convertFrom(6.7917, Feet));
-        SPEAKER_TARGET_POSITION = new Translation3d(SPEAKER_POSITION.getX(), SPEAKER_POSITION.getY(), SPEAKER_SHOT_POSITION.getZ());
+        SPEAKER_TARGET_POSITION = new Translation3d(SPEAKER_POSITION.getX() + Meters.convertFrom(2.5, Inches), SPEAKER_POSITION.getY() - Meters.convertFrom(2.5, Inches), SPEAKER_SHOT_POSITION.getZ());
         AMP_POSE = new Pose2d(red ? FIELD_WIDTH_METERS - Meters.convertFrom(72.5, Inches) : Meters.convertFrom(72.5, Inches), Meters.convertFrom(300, Inches), new Rotation2d(Math.PI / 2));;
         TRAP_STAGE_POSE = new Pose2d(red ? FIELD_WIDTH_METERS - 4.20 : 4.20, 2.90, Rotation2d.fromDegrees(red ? 120 : 60));
         TO_DRIVERSTATION = Rotation2d.fromDegrees(red ? 0 : 180);
