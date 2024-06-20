d00abaa73c2f66e714abbafc28e108958e698363
alohr86 <lohrandrew@icloud.com>
2024-06-20 16:54:51 -0400

    Test the intake I guess

--- src/main/java/frc/robot/subsystems/drivetrain/states/NoteLockState.java
+++ src/main/java/frc/robot/subsystems/drivetrain/states/NoteLockState.java
@@ -10,15 +10,20 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.drivers.ControllerDrivers;
 import frc.robot.subsystems.drivetrain.drivers.PositionalDrivers;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 
 public class NoteLockState extends AbstractDriveState<ControllerDrivers.YDriver, ControllerDrivers.XDriver, PositionalDrivers.ThetaDriver> {
-    public NoteLockState(Drivetrain drivetrain, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
+    IntakeSubsystem intake;
+
+    public NoteLockState(Drivetrain drivetrain, IntakeSubsystem intake, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
         super(
             drivetrain,
             new ControllerDrivers.YDriver(ySpeed),
             new ControllerDrivers.XDriver(xSpeed),
             new PositionalDrivers.ThetaDriver(drivetrain, new PIDController(3.5, 0.0001, 0))
         );
+
+        this.intake = intake;
     }
 
     @Override
@@ -36,6 +41,9 @@ public class NoteLockState extends AbstractDriveState<ControllerDrivers.YDriver,
         
         if(BACK_LIMELIGHT.getTV()) {
             thetaDriver.setDesiredAngle(Rotation2d.fromDegrees(BACK_LIMELIGHT.getTX()).minus(requiredSubsystem.getRobotPose().getRotation()).unaryMinus());
+            if(BACK_LIMELIGHT.getTY() < 0) {
+                intake.runMotor(0.8);
+            }
         } else {
             thetaDriver.setDesiredAngle(requiredSubsystem.getRobotPose().getRotation());
         }
