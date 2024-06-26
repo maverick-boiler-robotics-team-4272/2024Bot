56d08eb62ccf189997419e6cdfc7f506849bc606
Andy Lohr <lohrandrew@icloud.com>
2024-06-25 20:03:00 -0400

    It works with battery

--- src/main/java/frc/robot/commands/AutoNotePickup.java
+++ src/main/java/frc/robot/commands/AutoNotePickup.java
@@ -1,7 +1,6 @@
-package frc.robot.subsystems.drivetrain.states;
+package frc.robot.commands;
 
 import static frc.robot.constants.TelemetryConstants.Limelights.BACK_LIMELIGHT;
-import static frc.robot.constants.UniversalConstants.getGlobalPositions;
 
 import java.util.function.DoubleSupplier;
 
@@ -10,16 +9,24 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.drivers.ControllerDrivers;
 import frc.robot.subsystems.drivetrain.drivers.PositionalDrivers;
+import frc.robot.subsystems.drivetrain.states.AbstractDriveState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.shooter.Shooter;
 
-public class NoteLockState extends AbstractDriveState<ControllerDrivers.YDriver, ControllerDrivers.XDriver, PositionalDrivers.ThetaDriver> {
-    public NoteLockState(Drivetrain drivetrain, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
+public class AutoNotePickup extends AbstractDriveState<ControllerDrivers.YDriver, ControllerDrivers.XDriver, PositionalDrivers.ThetaDriver> {
+    IntakeSubsystem intake;
+    Shooter shooter;
+
+    public AutoNotePickup(Drivetrain drivetrain, IntakeSubsystem intake, Shooter shooter, DoubleSupplier xSpeed, DoubleSupplier ySpeed) {
         super(
             drivetrain,
             new ControllerDrivers.YDriver(ySpeed),
             new ControllerDrivers.XDriver(xSpeed),
             new PositionalDrivers.ThetaDriver(drivetrain, new PIDController(3.5, 0.0001, 0))
         );
+
+        this.intake = intake;
+        this.shooter = shooter;
     }
 
     @Override
@@ -37,8 +44,19 @@ public class NoteLockState extends AbstractDriveState<ControllerDrivers.YDriver,
         
         if(BACK_LIMELIGHT.getTV()) {
             thetaDriver.setDesiredAngle(Rotation2d.fromDegrees(BACK_LIMELIGHT.getTX()).minus(requiredSubsystem.getRobotPose().getRotation()).unaryMinus());
+            if(BACK_LIMELIGHT.getTY() < 0 && !shooter.lidarTripped()) {
+                intake.runMotor(0.9);
+                shooter.feed(0.9);
+            } else if(shooter.beginLidarTripped()) {
+                shooter.feed(0.7);
+            } else {
+                intake.runMotor(0);
+                shooter.feed(0.0);
+            }
         } else {
             thetaDriver.setDesiredAngle(requiredSubsystem.getRobotPose().getRotation());
+            intake.runMotor(0);
+            shooter.feed(0.0);
         }
 
         drive();
